45823828bd83f25edd4bb3b0f15d160e4d46c244
PereraAPD <161456406+PereraAPD@users.noreply.github.com>
2024-03-15 10:40:18 +0530

    beutify

--- ApplicationConsole.php
+++ ApplicationConsole.php
@@ -4,49 +4,99 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Loan Management Console</title>
+    <!-- Include stylesheet -->
     <link rel="stylesheet" href="style.css"> 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <!-- Include the common stylesheet -->
 </head>
 <body>
+    <!-- Main Header -->
     <header>
         <div class="logo-container">
-            <img src="logo.png" alt="Logo"> <!-- Replace with the actual path to your logo -->
-            <!-- <span>LMS</span> -->
+            <img src="logo.png" alt="Logo">
             <span>Main Menu</span>
         </div>
         <div class="title-container">
            <h2> Dashboard</h2>
         </div>
     </header>
-    
+    <!-- Loan Schemes Nav Bar -->
     <div class="navbar">
-        <!-- Navigation tabs for loan schemes -->
-        <a href="L01/LoanScheme1.php"><i class="fas fa-home"></i> <!-- Font Awesome icon for home -->
-            <span>General</span></a>
-        <a href="L02/LoanScheme2.php"><i class="fas fa-home"></i> <!-- Font Awesome icon for home --><span>Sonduru Piyasa</span></a>
-        <a href="L03/LoanScheme3.php"><i class="fas fa-home"></i> <!-- Font Awesome icon for home --><span>Govi Navoda</span></a>
-        <a href="L04/LoanScheme4.php"><i class="fas fa-home"></i> <!-- Font Awesome icon for home --><span>Riya Shakthi</span></a>
-        <a href="L05/LoanScheme5.php"><i class="fas fa-home"></i> <!-- Font Awesome icon for home --><span>Ran Aswenna</span></a>
-        <a href="L06/LoanScheme6.php"><i class="fas fa-home"></i> <!-- Font Awesome icon for home --><span>Jaya Isura</span></a>
-        <a href="L07/LoanScheme7.php"><i class="fas fa-home"></i> <!-- Font Awesome icon for home --><span>Rivi Bala Savi</span></a>
-        <a href="L08/LoanScheme8.php"><i class="fas fa-home"></i> <!-- Font Awesome icon for home --><span>Diri Saviya</span></a>
-        <a href="L09/LoanScheme9.php"><i class="fas fa-home"></i> <!-- Font Awesome icon for home --><span>Madya Aruna</span></a>
-        <a href="L10/LoanScheme10.php"><i class="fas fa-home"></i> <!-- Font Awesome icon for home --><span>Green Loan</span></a>
-        <a href="L11/LoanScheme11.php"><i class="fas fa-home"></i> <!-- Font Awesome icon for home --><span>Home Sweet Home</span></a>
-        <a href="L12/LoanScheme12.php"><i class="fas fa-home"></i> <!-- Font Awesome icon for home --><span>Sihina Maligaya</span></a>
-        <a href="L13/LoanScheme13.php"><i class="fas fa-home"></i> <!-- Font Awesome icon for home --><span>Rekawaranaya</span></a>
-        <a href="L14/LoanScheme14.php"><i class="fas fa-home"></i> <!-- Font Awesome icon for home --><span>Mini Taxi</span></a>
-        <a href="L15/LoanScheme15.php"><i class="fas fa-home"></i> <!-- Font Awesome icon for home --><span>Sigithi Pasala</span></a>
-        <a href="L16/LoanScheme16.php"><i class="fas fa-home"></i> <!-- Font Awesome icon for home --><span>Working Capitol</span></a>
-        <a href="L17/LoanScheme17.php"><i class="fas fa-home"></i> <!-- Font Awesome icon for home --><span>City Ride</span></a>
-        <a href="L18/LoanScheme18.php"><i class="fas fa-home"></i> <!-- Font Awesome icon for home --><span>My Future</span></a>
-        <a href="security.php"><i class="fas fa-shield-alt"></i> <span>Security</span></a>
-
-        <!-- Other tabs -->
-        
+        <a href="L01/LoanScheme1.php">
+            <i class="fas fa-home"></i>
+            <span>General</span>
+        </a>
+        <a href="L02/LoanScheme2.php">
+            <i class="fas fa-home"></i> <span>Sonduru Piyasa</span>
+        </a>
+        <a href="L03/LoanScheme3.php">
+            <i class="fas fa-home"></i>
+            <span>Govi Navoda</span>
+        </a>
+        <a href="L04/LoanScheme4.php">
+            <i class="fas fa-home"></i>
+            <span>Riya Shakthi</span>
+        </a>
+        <a href="L05/LoanScheme5.php">
+            <i class="fas fa-home"></i>
+            <span>Ran Aswenna</span>
+        </a>
+        <a href="L06/LoanScheme6.php">
+            <i class="fas fa-home"></i>
+            <span>Jaya Isura</span>
+        </a>
+        <a href="L07/LoanScheme7.php">
+            <i class="fas fa-home"></i>
+            <span>Rivi Bala Savi</span>
+        </a>
+        <a href="L08/LoanScheme8.php">
+            <i class="fas fa-home"></i>
+            <span>Diri Saviya</span>
+        </a>
+        <a href="L09/LoanScheme9.php">
+            <i class="fas fa-home"></i>
+            <span>Madya Aruna</span>
+        </a>
+        <a href="L10/LoanScheme10.php">
+            <i class="fas fa-home"></i>
+            <span>Green Loan</span>
+        </a>
+        <a href="L11/LoanScheme11.php">
+            <i class="fas fa-home"></i><span>Home Sweet Home</span>
+        </a>
+        <a href="L12/LoanScheme12.php">
+            <i class="fas fa-home"></i>
+            <span>Sihina Maligaya</span>
+        </a>
+        <a href="L13/LoanScheme13.php">
+            <i class="fas fa-home"></i>
+            <span>Rekawaranaya</span
+        </a>
+        <a href="L14/LoanScheme14.php">
+            <i class="fas fa-home"></i>
+            <span>Mini Taxi</span>
+        </a>
+        <a href="L15/LoanScheme15.php">
+            <i class="fas fa-home"></i>
+            <span>Sigithi Pasala</span>
+        </a>
+        <a href="L16/LoanScheme16.php">
+            <i class="fas fa-home"></i>
+            <span>Working Capitol</span>
+        </a>
+        <a href="L17/LoanScheme17.php">
+            <i class="fas fa-home"></i>
+            <span>City Ride</span>
+        </a>
+        <a href="L18/LoanScheme18.php">
+            <i class="fas fa-home"></i>
+            <span>My Future</span>
+        </a>
+        <a href="security.php">
+            <i class="fas fa-shield-alt"></i> <span>Security</span>
+        </a>
+        <!-- Add other tabs if needed -->
     </div>
-
+    <!-- Main Page Content -->
     <div class="content">
         <div id="content-box">
     <div class="welcometext">
@@ -61,25 +111,27 @@
             <address>
                 The Secretariat, Colombo 01, Sri Lanka.<br>
             <table>
-            <tr>
-                <th>Phone:</th>
-                <th>Fax:</th>
-</tr><tr>
-                <td>+94 112 484 500<br>+94 112 484 600<br>+94 112 484 700</td>
+                <tr>
+                    <th>Phone:</th>
+                    <th>Fax:</th>
+                </tr>
+                <tr>
+                    <td>+94 112 484 500<br>+94 112 484 600<br>+94 112 484 700</td>
                 <td>+94 112 449 823</td>
-            </tr>
-            <tr>
-                <th>Email:</th>
-                <td><a href="mailto:info@mo.treasury.gov.lk">info@mo.treasury.gov.lk</a></td>
-            </tr>
-        </table>
+                </tr>
+                <tr>
+                    <th>Email:</th>
+                    <td><a href="mailto:info@mo.treasury.gov.lk">info@mo.treasury.gov.lk</a></td>
+                </tr>
+            </table>
             </address>
         </div>
         <div class="contact-box">
             <h3>Department of Development Finance</h3>
             <hr>
-            <address>
-                Room No. 140, 1st Floor, Ministry of Finance, The Secretariat,Colombo 01. <table>
+                <address>
+                    Room No. 140, 1st Floor, Ministry of Finance, The Secretariat,Colombo 01. 
+                <table>
                     <tr>
                         <th>Phone:</th>
                         <th>Fax:</th>
@@ -93,38 +145,25 @@
                         <td><a href="mailto:dgdf@dfd.treasury.gov.lk">dgdf@dfd.treasury.gov.lk</a></td>
                     </tr>
                 </table>
-            </address>
+                </address>
+            </div>
         </div>
     </div>
-    </div>
 </div>
-
-
-    
-    <!-- </div> 
-        <div id="logout">
-         Add a link or button for logging out 
-        <a href="logout.php">Logout</a>
-    </div>
-
-    <div id="security">
-        <!-- Add a link or button for accessing the security page
-        <a href="SecurityPage.php">Security</a>
-    </div> -->
-    <footer>
+<!-- Main Footer -->
+<footer>
     <div class="footer-left">
             <a href="user.php"> 
-                <i class="fas fa-user"></i> <!-- user icon --> User
+                <i class="fas fa-user"></i>  User
             </a>
             <a href="logout.php"> 
-                <i class="fas fa-sign-out-alt"></i> <!-- logout icon --> Logout
+                <i class="fas fa-sign-out-alt"></i>
+                Logout
             </a>
     </div>
     <div class="footer-right">
-        <!-- Rest of the footer content -->
         <p>&copy; 2024 Loan Management System. All rights reserved.</p>
     </div>
 </footer>
-
 </body>
 </html>
